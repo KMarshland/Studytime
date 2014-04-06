af78794083771f4caead23ed414e40360b0a9b71
Kai Marshland <kaimarshland@gmail.com>
2014-04-06 16:15:36 -0400

    Updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Studytime::Application.routes.draw do
+  get "static_pages/home"
   resources :studygroups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => 'static_pages#home'
+  # root 'static_pages_home_path'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
