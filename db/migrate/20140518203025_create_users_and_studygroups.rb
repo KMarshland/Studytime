3224eb01f3bdf60fed5b822df27f13c515349650
System Administrator <root@unknown00236c93b993.gateway.2wire.net>
2014-05-19 20:45:32 -0700

    Heroku fixes

--- db/migrate/20140518203025_create_users_and_studygroups.rb
+++ db/migrate/20140518203025_create_users_and_studygroups.rb
@@ -1,12 +1,12 @@
 class CreateUsersAndStudygroupsTable < ActiveRecord::Migration
   def self.up
-    create_table :studygroups_users do |t|
-      t.integer :studygroup_id
-      t.integer :user_id
-    end
+    #create_table :studygroups_users do |t|
+    #  t.integer :studygroup_id
+    #  t.integer :user_id
+    #end
   end
 
   def self.down
-    drop_table :studygroups_users
+    #drop_table :studygroups_users
   end
 end
