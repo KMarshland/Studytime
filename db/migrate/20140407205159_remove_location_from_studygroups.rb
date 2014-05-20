dcfad7fe9c8ebf59e526e836c4ce82d12c4b11f3
Kai Marshland <kaimarshland@gmail.com>
2014-05-19 20:31:01 -0700

    heroku

--- db/migrate/20140407205159_remove_location_from_studygroups.rb
+++ db/migrate/20140407205159_remove_location_from_studygroups.rb
@@ -1,9 +1,9 @@
 class RemoveLocationFromStudygroups < ActiveRecord::Migration
   def up
-    remove_column :studygroups, :location
+    #remove_column :studygroups, :location
   end
 
   def down
-    add_column :studygroups, :location, :string
+    #add_column :studygroups, :location, :string
   end
 end
--- db/migrate/20140407205921_add_adress_to_studygroup.rb
+++ db/migrate/20140407205921_add_adress_to_studygroup.rb
@@ -1,19 +1,19 @@
 class AddAdressToStudygroup < ActiveRecord::Migration
 
   def up
-    add_column :studygroups, :street_number, :integer
-    add_column :studygroups, :street_name, :string
-    add_column :studygroups, :city, :string
-    add_column :studygroups, :state, :string
-    add_column :studygroups, :zip_code, :integer
+   # add_column :studygroups, :street_number, :integer
+   # add_column :studygroups, :street_name, :string
+   # add_column :studygroups, :city, :string
+   # add_column :studygroups, :state, :string
+   # add_column :studygroups, :zip_code, :integer
   end
 
   def down
-    remove_column :studygroups, :street_number
-    remove_column :studygroups, :street_name
-    remove_column :studygroups, :city
-    remove_column :studygroups, :state
-    remove_column :studygroups, :zip_code
+    #remove_column :studygroups, :street_number
+    #remove_column :studygroups, :street_name
+    #remove_column :studygroups, :city
+    #remove_column :studygroups, :state
+    #remove_column :studygroups, :zip_code
   end
 
 end
